e759f9b08c2a7d6fb90f9cff82062e21d056ae1f
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2022-07-30 16:10:27 -0600

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,50 @@
+Param (
+[Parameter(Mandatory=$false, HelpMessage="The version the mod should be compiled with")][Alias("ver")][string]$Version,
+[Parameter(Mandatory=$false, HelpMessage="Switch to create a clean compilation")][Alias("rebuild")][Switch]$clean,
+[Parameter(Mandatory=$false, HelpMessage="To create a release build")][Alias("publish")][Switch]$release,
+[Parameter(Mandatory=$false, HelpMessage="To create a github actions build, assumes specific Environment variables are set")][Alias("github-build")][Switch]$actions,
+[Parameter(Mandatory=$false, HelpMessage="To create a debug build (Does not strip other libs)")][Alias("debug-build")][Switch]$debugbuild
+)
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+$QPMpackage = "./qpm.json"
+$qpmjson = Get-Content $QPMpackage -Raw | ConvertFrom-Json
+$ModID = $qpmjson.info.id
+if (-not $Version) {
+    $VERSION = $qpmjson.info.version
+} else {
+    $VERSION = $Version
+}
+if ($release -ne $true -and -not $VERSION.Contains('-Dev')) {
+    $VERSION += "-Dev"
+}
+
+if ($env:version -eq "") {
+    & qpm-rust package edit --version $VERSION
+}
+
+echo "Building mod $ModID version $VERSION"
+
+if ($clean.IsPresent)
+{
+    if (Test-Path -Path "build")
+    {
+        remove-item build -R
+    }
+}
+
+if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
+{
+    $out = new-item -Path build -ItemType Directory
+}
+
+cd build
+if ($debugbuild -eq $true) {
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DDEBUG=true ../
+} else {
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
+}
+& cmake --build . -j 6
+$ExitCode = $LastExitCode
+cd ..
+exit $ExitCode
+echo Done
